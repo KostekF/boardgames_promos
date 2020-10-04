9f7539ea7e7a197c5b98ebbd744690bfd2d1f738
KostekF <filip.kostecki@student.put.poznan.pl>
2020-10-04 22:27:52 +0200

    Add discord msgs function

---
+++ discord_msgs.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def send_msg(webhookurl, message="@here Your info is here!", *, embed=None, bot_name='temp'):
+    data = {}
+    data['content'] = message
+    data['username'] = bot_name
+    if embed:
+        data['embeds'] = []
+        data['embeds'].append(embed)
+
+    result = requests.post(webhookurl, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        # print("Payload delivered successfully, code {}.".format(result.status_code))
+        pass
