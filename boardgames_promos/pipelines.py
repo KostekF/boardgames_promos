fb3363ba2a81567b4722552c8a8662540dbef45a
KostekF <filipfilc@gmail.com>
2020-12-10 17:44:58 +0100

    Add filtering based on blacklisted words and prices

--- boardgames_promos/pipelines.py
+++ boardgames_promos/pipelines.py
@@ -5,16 +5,19 @@ import logging
 from itemadapter import ItemAdapter
 
 import discord_msgs as dsc
+import settings_offers
 
 
 class DiscordSenderPipeline:
-
     def __init__(self):
         self.save_promo_to_file = False
         self.first_run = False
         self.promo_dicts = []
         self.promo_dict = {}
         self.filename = './promos_scraped.jl'
+
+        self.words_blacklist = settings_offers.words_blacklist
+        self.price_range = settings_offers.price_range
 
         if os.environ.get('DSC_WEBHOOK') is None:
             import config
@@ -37,19 +40,32 @@ class DiscordSenderPipeline:
                     writer.write(promo)
 
     def process_item(self, item, spider):
+        promo_first_seen = True
         send_promo = True
         self.promo_dict = ItemAdapter(item).asdict()
 
-        #if promo was scraped before discard it
+        #if promo was scraped before - discard it
         with jsonlines.open('promos_scraped.jl') as reader:
             for obj in reader:
                 if self.promo_dict['promo_id'] == obj['promo_id']:
-                    send_promo = False
+                    promo_first_seen = False
 
-        if send_promo:
+        #if promo contains blacklisted word - discard it
+        for word in self.words_blacklist:
+            if word in self.promo_dict['title'].lower():
+                send_promo = False
+
+        #if promo price is not in min<price<max and price is not None - discard it
+        if self.promo_dict['price'] is not None:
+            promo_price = float(self.promo_dict['price'][:-2].replace(',', '.'))
+            if not (self.price_range['min'] < promo_price < self.price_range['max']):
+                send_promo = False
+
+        if promo_first_seen:
             self.save_promo_to_file = True
             self.promo_dicts.append(self.promo_dict)
-            if not self.first_run:
+
+            if send_promo and not self.first_run:
                 dsc.send_promo_msg(self.dsc_webhook, self.promo_dict)
 
         return item
---
+++ settings_offers.py
@@ -0,0 +1,5 @@
+# offers containing blacklisted words won't be sent
+words_blacklist = ['monopoly', 'auchan', 'puzzle']
+
+# offers only within price range will be sent
+price_range = {'min': 65, 'max': float('inf')}
